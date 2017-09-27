51c228d70718f04eafbd560a825513fdee53a0db
hana4 <hana4@DESKTOP-P4D391Q>
2017-09-27 15:37:42 +0900

    ..

--- WebClass/WebContent/jsp/sessionInfo.jsp
+++ WebClass/WebContent/jsp/sessionInfo.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@page import = "org.dimigo.vo.UserVO" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<%
+	UserVO vo = (UserVO) session.getAttribute("user");
+%>
+<h1>id = <%= vo.getId() %></h1>
+<h1>name : <%= vo.getName() %></h1>
+<h1>nickname : <%= vo.getNickname()%></h1>
 </body>
 </html>
--- WebClass/src/org/dimigo/servlet/SessionServlet.java
+++ WebClass/src/org/dimigo/servlet/SessionServlet.java
@@ -1,6 +1,8 @@
 package org.dimigo.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +28,17 @@ public class SessionServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// 세션에 사용자 정보가 없으면 login.jsp로 포워딩
+		// 만약에 있으면 sessionInfo.jps로 포워딩
+		//sessionInfo.jsp에서는 세션에 담긴 사용자 정보(id name nickname)출력
+		if(request.getSession().getAttribute("user") == null){
+			response.sendRedirect("jsp/login.jsp");
+			
+		}
+		else{
+			RequestDispatcher rd = request.getRequestDispatcher("jsp/session.jsp");
+		rd.forward(request, response);
+		}
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
